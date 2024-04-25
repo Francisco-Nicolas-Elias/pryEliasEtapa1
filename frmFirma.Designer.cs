0b4de1933f989501131d592d4e24c8387a10f33b
Francisco Elias <franeliasbv@gmail.com>
2024-04-25 19:51:45 -0300

    Agregar archivos de proyecto.

---
+++ frmFirma.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace pryEliasEtapa1
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbFirma = new System.Windows.Forms.GroupBox();
+            this.lblNombreCliente = new System.Windows.Forms.Label();
+            this.txtNombreFirma = new System.Windows.Forms.TextBox();
+            this.pbFirma = new System.Windows.Forms.PictureBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.gbFirma.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbFirma
+            // 
+            this.gbFirma.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gbFirma.Controls.Add(this.pbFirma);
+            this.gbFirma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbFirma.Location = new System.Drawing.Point(15, 55);
+            this.gbFirma.Name = "gbFirma";
+            this.gbFirma.Size = new System.Drawing.Size(407, 233);
+            this.gbFirma.TabIndex = 0;
+            this.gbFirma.TabStop = false;
+            this.gbFirma.Text = "Firme aquí:";
+            // 
+            // lblNombreCliente
+            // 
+            this.lblNombreCliente.AutoSize = true;
+            this.lblNombreCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreCliente.Location = new System.Drawing.Point(12, 21);
+            this.lblNombreCliente.Name = "lblNombreCliente";
+            this.lblNombreCliente.Size = new System.Drawing.Size(139, 16);
+            this.lblNombreCliente.TabIndex = 1;
+            this.lblNombreCliente.Text = "Ingrese su nombre:";
+            // 
+            // txtNombreFirma
+            // 
+            this.txtNombreFirma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreFirma.Location = new System.Drawing.Point(154, 17);
+            this.txtNombreFirma.Name = "txtNombreFirma";
+            this.txtNombreFirma.Size = new System.Drawing.Size(190, 22);
+            this.txtNombreFirma.TabIndex = 2;
+            this.txtNombreFirma.TextChanged += new System.EventHandler(this.txtNombreFirma_TextChanged);
+            // 
+            // pbFirma
+            // 
+            this.pbFirma.BackColor = System.Drawing.SystemColors.Control;
+            this.pbFirma.Location = new System.Drawing.Point(9, 21);
+            this.pbFirma.Name = "pbFirma";
+            this.pbFirma.Size = new System.Drawing.Size(392, 206);
+            this.pbFirma.TabIndex = 0;
+            this.pbFirma.TabStop = false;
+            this.pbFirma.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbFirma_MouseDown);
+            this.pbFirma.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbFirma_MouseMove);
+            this.pbFirma.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbFirma_MouseUp);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(216, 294);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(97, 34);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(319, 294);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(97, 34);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // frmFirma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(466, 351);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtNombreFirma);
+            this.Controls.Add(this.lblNombreCliente);
+            this.Controls.Add(this.gbFirma);
+            this.Name = "frmFirma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firma";
+            this.gbFirma.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirma)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbFirma;
+        private System.Windows.Forms.PictureBox pbFirma;
+        private System.Windows.Forms.Label lblNombreCliente;
+        private System.Windows.Forms.TextBox txtNombreFirma;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
